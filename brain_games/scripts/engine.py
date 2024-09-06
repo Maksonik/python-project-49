36581f704e5628b1e6c2f8b71941e6864dce2f22
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2024-09-06 17:21:41 +0500

    a

--- brain_games/scripts/engine.py
+++ brain_games/scripts/engine.py
@@ -18,7 +18,8 @@ def play_game(game, description):
             print('Correct!')
             correct_answers += 1
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{answer}' is wrong answer"
+                  f" ;(. Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
 
